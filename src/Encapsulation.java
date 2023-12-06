b9a6866432db2558e2577f71df0347edbf8fa199
Kanisorn Sa-nguansook <kanisorn.san@outlook.com>
2023-12-06 17:26:33 +0700

    try using new class

---
+++ src/Encapsulation.java
@@ -0,0 +1,5 @@
+public class Encapsulation {
+    public void showmessage() {
+        System.out.println("Hello from another class");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -13,5 +13,8 @@ public class Main {
             // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
+        Encapsulation encapsulation = new Encapsulation();
+        encapsulation.showmessage();
+
     }
 }
